dae25825821e790feec55f2a1956e98bd1d245a1
Sandeep .A <Sandeep .A>
2017-01-21 22:45:37 +0530

    Working on page

---
+++ js/script.js
@@ -0,0 +1,8 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.slider').slider({full_width: true});
+    $('.slider').slider({height:500});
+  }); // end of document ready
+})(jQuery); 
